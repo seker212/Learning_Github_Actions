bdba5db716f99955bd5ee6fa0730ae071d737d22
seker212 <seker212@gmail.com>
2023-07-04 19:56:36 +0200

    base code

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var rng = new Random();
+var nr = new[] { rng.Next(), rng.Next(), rng.Next() };
+Console.WriteLine(ToStr(nr.AsEnumerable()));
+
+string ToStr<T>(IEnumerable<T> list)
+    => $"[{string.Join(',', list)}]";
