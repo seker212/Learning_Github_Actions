375b6207dcdd1a64ad217452524b5bebc0c71bfd
seker212 <seker212@gmail.com>
2023-07-04 21:10:39 +0200

    new ToStr

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,5 +4,8 @@ var rng = new Random();
 var nr = new[] { rng.NextDouble(), rng.NextDouble(), rng.NextDouble() };
 Console.WriteLine(ToStr(nr.AsEnumerable()));
 
-string ToStr(IEnumerable<double> list)
+string ToStrOld(IEnumerable<double> list)
+    => $"[{string.Join(',', list)}]";
+
+string ToStr(IEnumerable<string> list)
     => $"[{string.Join(',', list)}]";
