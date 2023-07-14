4e7fa134ed9980a20bccad42c2b759fa32f5d0a3
seker212 <seker212@gmail.com>
2023-07-14 20:03:56 +0200

    buildable version

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,10 +2,7 @@
 Console.WriteLine("Hello, World!");
 var rng = new Random();
 var nr = new[] { rng.NextDouble(), rng.NextDouble(), rng.NextDouble() };
-Console.WriteLine(ToStr(nr.AsEnumerable()));
+Console.WriteLine(ToStrOld(nr.AsEnumerable()));
 
 string ToStrOld(IEnumerable<double> list)
-    => $"[{string.Join(',', list)}]";
-
-string ToStr(IEnumerable<string> list)
     => $"[{string.Join(',', list)}]";
